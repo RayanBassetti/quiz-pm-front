0f258b06026c72aed8a6ec6a28d573ca8cf677d6
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2020-11-01 16:23:42 +0100

    update : fix temporaire pour gérer les différents types de bouttons

--- src/components/common/QuizButton.js
+++ src/components/common/QuizButton.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
 
-function QuizButton({text, text_bold, smiley, route}) {
+function QuizButton({text, text_bold, smiley, route, type}) {
 
     const history = useHistory()
 
     const handleClick = (route) => {
         history.push(route)
     }
+
+    const handleSubmit = (content) => {
+        console.log(content)
+    }
     
     return (
+        <>
+        {smiley &&
         <button class="button-default" onClick={() => handleClick(route)}>{text} <span class="font-i font-b font-up">{text_bold}</span> {smiley}</button>
+        }
+        {type === "submit" &&
+        <button class="button-default" onClick={() => handleSubmit(route)}>{text} <span class="font-i font-b font-up">{text_bold}</span> {smiley}</button>
+        }
+        </>
     )
 }
 
--- src/components/ending/Ending.js
+++ src/components/ending/Ending.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import QuizButton from '../common/QuizButton'
 
 function Ending() {
     return (
-        <h1>Quiz ended ! </h1>
+        <div>
+            <p class="font-s-24 font-w-300">Nos félicitations ! </p>
+            <p class="font-s-24 font-w-300">On peut dire que vous êtes réellement <span class="font-up font-i font-w-700">badass</span>.</p>
+            <TextField id="outlined-basic" label="Say my name..." variant="outlined" />
+            <QuizButton text={"Enregistrer"} type="submit"/>
+            <div class="accueil-choix">
+                <a href="/leaderboard">Voir mes réponses en détail</a>
+            </div>
+        </div>
     )
 }
 
