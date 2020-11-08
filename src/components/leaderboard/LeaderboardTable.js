ead92266642d98188ab8933f03edac53f30bee5d
Nicolas Resmi <n.resmi@etu-webschoolfactory.fr>
2020-11-08 17:02:22 +0100

    css Leaderboard

---
+++ src/components/leaderboard/LeaderboardTable.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function LeaderboardTable() {
+    return (
+        <>
+            <table>
+                <tr>
+                    <th>#1 &#129351;</th>
+                    <th>pseudo</th>
+                    <th class="font-up font-b font-i">BADASS</th>
+                    <th>11/10</th>
+                </tr>
+                <tr>
+                    <th>#2 &#129352;</th>
+                    <th>pseudo</th>
+                    <th class="font-up font-b font-i">BADASS</th>
+                    <th>10/10</th>
+                </tr>
+                <tr>
+                    <th>#3 &#129353;</th>
+                    <th>pseudo</th>
+                    <th class="font-up font-b font-i"></th>
+                    <th>9/10</th>
+                </tr>
+                <tr>
+                    <th>#4</th>
+                    <th>pseudo</th>
+                    <th class="font-up font-b font-i"></th>
+                    <th>7/10</th>
+                </tr>
+            </table>
+        </>
+    )
+}
+
+export default LeaderboardTable
